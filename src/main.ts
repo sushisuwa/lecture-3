b0257724f50f70402da10cde4ec2321f28daedf1
Adam Smith <adam@adamsmith.as>
2024-10-02 15:32:51 -0700

    enhanced features

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "My sweet game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+function boost_score(score: number) {
+  return score + Math.sqrt(score);
+}
+
+let num_clicks = 0;
+const button = document.createElement("button");
+button.innerHTML = "Click me!";
+button.onclick = () => {
+  num_clicks = boost_score(num_clicks);
+  button.innerHTML = `${num_clicks} clicks so far`;
+};
+app.append(button);
